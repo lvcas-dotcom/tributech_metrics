61f08571d4f031587f872b525d0a042c95d1a039
Gabriel Arantes <bielarantes00oo@gmail.com>
2025-10-03 16:59:57 -0300

    feat: adicionando o componente de grafico de barras

--- frontend/src/app/features/feature/home-page/home-page.component.ts
+++ frontend/src/app/features/feature/home-page/home-page.component.ts
@@ -4,10 +4,12 @@ import { SidebarComponent } from '../../../core/layout/sidebar/sidebar.component
 import { DataCardComponent } from '../../../shared/ui/data-card/data-card.component'
 
 import { PieGraphicComponent } from '../../../shared/ui/pie-graphic/pie-graphic.component'
+import { BarGraphicComponent } from '../../../shared/ui/bar-graphic/bar-graphic.component'
+
 @Component({
     selector: 'app-home-page',
     imports: [ SidebarComponent,
-        DataCardComponent, PieGraphicComponent
+        DataCardComponent, PieGraphicComponent,BarGraphicComponent
     ],
     standalone: true,
     templateUrl: './home-page.component.html',
