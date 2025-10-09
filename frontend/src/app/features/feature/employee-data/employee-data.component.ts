c2038e61b641afaa6e21b9a3beb37ba1a22ddb02
Gabriel Arantes <bielarantes00oo@gmail.com>
2025-10-09 17:24:38 -0300

    feat: adicionando card de usuarios e informações

--- frontend/src/app/features/feature/employee-data/employee-data.component.ts
+++ frontend/src/app/features/feature/employee-data/employee-data.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../../../core/layout/sidebar/sidebar.component';
+import { DataCardComponent } from '../../../shared/ui/data-card/data-card.component';
 
 @Component({
   selector: 'app-employee-data',
-  imports: [SidebarComponent],
+  standalone: true,
+  imports: [SidebarComponent,DataCardComponent],
   templateUrl: './employee-data.component.html',
   styleUrl: './employee-data.component.scss'
 })
