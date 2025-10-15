6e56b6dc86a9c154753b3ef4538dcfa48f618492
Gabriel Arantes <bielarantes00oo@gmail.com>
2025-10-15 10:25:20 -0300

    alteracoes visuais

--- frontend/src/app/features/data-acess/services/users-service.service.ts
+++ frontend/src/app/features/data-acess/services/users-service.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { environment } from "../../../environments/environment";
 
@@ -8,4 +8,16 @@ import { environment } from "../../../environments/environment";
 export class UserService{
     private http = inject(HttpClient);
     private api = environment.apiUrl;
+
+    getHoursByUser(users: string,startDate?: string, endDate?: string,projects?: string){
+        let params = new HttpParams().set('users',users);
+        if(startDate) params = params.set('start_date',startDate);
+        
+        if(endDate) params = params.set('end_date',endDate);
+        
+        if(projects) params = params.set('projects',projects);
+        
+        return this.http.get<{usuario:string, horas_ajuda:number,horas_totais_mes:number,horas_liquidas:number}>(
+            `${this.api}/metrics/hours-by-user`, {params});
+    }
 }
